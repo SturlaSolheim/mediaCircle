c090c8a759f2acf31edea1704869b888fffc2f24
Sturla Solheim <solheimsturla@protonmail.com>
2025-11-06 18:35:37 +0100

    implementert se alle album og legg til album

--- handlers/albums.go
+++ handlers/albums.go
@@ -30,8 +30,39 @@ func GetAlbum(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateAlbum(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	json.NewEncoder(w).Encode(models.Response{Message: "Ikke implementert enda", Status: 204})
+	var album models.Album
+	err := json.NewDecoder(r.Body).Decode(&album)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.Response{
+			Message: "Invalid request",
+			Status:  400,
+		})
+		return
+	}
+	
+	if album.Name == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(models.Response{
+			Message: "Album name is required",
+			Status:  400,
+		})
+		return
+	}
+	
+	repo := database.NewAlbumRepository()
+	err = repo.Save(&album)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(models.Response{
+			Message: "En feil skjedde",
+			Status:  500,
+		})
+		return
+	}
+	
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(album)
 }
 
 func UpdateAlbum(w http.ResponseWriter, r *http.Request) {
--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ func SetupRoutes(r chi.Router) {
 		r.Get("/health", handlers.HealthCheck)
 		r.Route("/albums", func(r chi.Router) {
 			r.Get("/", handlers.GetAlbums)
-			// r.Post("/", handlers.CreateAlbum)
+			r.Post("/", handlers.CreateAlbum)
 			// r.Get("/{id}", handlers.GetAlbum)
 			// r.Put("/{id}", handlers.UpdateAlbum)
 			// r.Delete("/{id}", handlers.DeleteAlbum)
